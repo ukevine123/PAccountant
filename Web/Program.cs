00e74d0f76207fceaa8cd355cf953bed683cccab
honyhonorine-lgml <honyhonorine@gmail.com>
2026-02-21 22:37:21 +0200

    Initial commit: Fixed project structure and MudBlazor setup

---
+++ Web/Program.cs
@@ -0,0 +1,41 @@
+using Web.Components;
+using MudBlazor.Services;
+
+// using Application.Services.Accounts;
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMudServices();//mudblazor services for UI components
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+// / Application Services
+//  builder.Services.AddScoped<IAccountService, AccountService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
